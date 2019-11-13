ad1705115062f5538a304b27f50a7ce27e8f5f66
gallimoff <galimov-azat@bk.ru>
2019-11-13 13:50:45 +0300

    JdbcTemplate

--- src/main/java/ru/itpark/servlet/CatalogServlet.java
+++ src/main/java/ru/itpark/servlet/CatalogServlet.java
@@ -10,11 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class CatalogServlet extends HttpServlet {
     private AutoService autoService;
     private FileService fileService;
+
     @Override
     public void init() throws ServletException {
         InitialContext context = null;
@@ -30,29 +30,17 @@ public class CatalogServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
             req.setAttribute("items", autoService.getAll());
             req.getRequestDispatcher("/WEB-INF/catalog.jsp").forward(req , resp);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ServletException(e);
-        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
             var name = req.getParameter("name");
             var description = req.getParameter("description");
             var part = req.getPart("image");
-
             var image = fileService.writeFile(part);
-
             autoService.create(name, description, image);
             resp.sendRedirect(String.join("/", req.getContextPath(), req.getServletPath()));
-        } catch (SQLException e ) {
-            e.printStackTrace();
-            throw new ServletException(e);
-        }
     }
 }
